a6beb2933477d7028848e3a095e8a13851636ca2
Serina Wu <is.serina.wu@gmail.com>
2024-04-23 16:19:19 +0800

    -fix axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import config from '@/config/index.js';
 
-const baseUrl = process.env.NODE_ENV === 'development' ? config.baseUrl.dev : config.baseUrl.pro;
+const baseUrl = process.env.USE_MOCK_API === 'true' ? config.baseUrl.dev : config.baseUrl.pro;
 
 class HttpRequest {
     constructor() {
